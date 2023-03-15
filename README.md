e79c046bc4bd4b5c6cb90c9edf0c05587487b6de
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2023-03-15 22:08:16 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Introweb
+# Introweb
---
+++ active.js
@@ -0,0 +1,16 @@
+function infoFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+function degreeFunction() {
+    document.body.scrollWidth = 1100; // For Safari
+    document.documentElement.scrollTop = 1100; // For Chrome, Firefox, IE and Opera
+}
+function pjFunction() {
+    document.body.scrollWidth = 1600; // For Safari
+    document.documentElement.scrollTop = 1600; // For Chrome, Firefox, IE and Opera
+}
+function aloFunction() {
+    document.body.scrollTop = 3500; // For Safari
+    document.documentElement.scrollTop = 3500; // For Chrome, Firefox, IE and Opera
+}
